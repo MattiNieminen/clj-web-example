c9d7c49c43149038e1ba43d3dd3f7a8116dbf5ae
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-05-30 00:32:11 +0300

    Added logging dependencies and initial logback.xml file to the project.

--- app/project.clj
+++ app/project.clj
@@ -4,6 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [ch.qos.logback/logback-classic "1.1.3"]
                  [com.stuartsierra/component "0.2.3"]
                  [metosin/potpuri "0.2.2"]
                  [http-kit "2.1.19"]]
--- app/src/clj_web_example/component/config.clj
+++ app/src/clj_web_example/component/config.clj
@@ -1,7 +1,8 @@
 (ns clj-web-example.component.config
   (:require [potpuri.core :as potpuri]
             [clojure.java.io :as io]
-            [com.stuartsierra.component :as component]))
+            [com.stuartsierra.component :as component]
+            [clojure.tools.logging :as log]))
 
 (defn import-config
   [profile]
@@ -14,7 +15,7 @@
   component/Lifecycle
   
   (start [this]
-    (println "Loading configuration with profile" profile)
+    (log/info "Loading configuration with profile" profile)
     (assoc this :loaded (import-config profile)))
   
   (stop [this]
