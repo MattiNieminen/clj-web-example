31d9c0f39f9f7de6c61408936e72e3b70cfe33c6
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-05-29 18:58:33 +0300

    Created Clojure project with Leiningen.

---
+++ app/test/clj_web_example/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-web-example.core-test
+  (:require [clojure.test :refer :all]
+            [clj-web-example.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
