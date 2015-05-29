04bf903d64de32cacc7846183b76649efe8e6285
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-05-29 19:50:45 +0300

    Added http kit dependency to the project.

--- app/project.clj
+++ app/project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.stuartsierra/component "0.2.3"]]
+                 [com.stuartsierra/component "0.2.3"]
+                 [http-kit "2.1.19"]]
   :main ^:skip-aot clj-web-example.main
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
