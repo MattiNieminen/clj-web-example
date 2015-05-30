1055a3419f25e57531e157d35a9c04ffdecdbc8f
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-05-30 16:22:07 +0300

    Implemented initial Reagent component.

--- app/src/clj_web_example/ui/main.cljs
+++ app/src/clj_web_example/ui/main.cljs
@@ -1,3 +1,13 @@
-(ns clj-web-example.ui.main)
+(ns ^:figwheel-always clj-web-example.ui.main
+  (:require [reagent.core :as reagent]))
 
-(.log js/console "Here we go!!!")
+(defn simple-component []
+  [:div
+   [:h1 "Hello World!"]])
+
+(defn init!
+  []
+  (.log js/console "Here we go! Rendering the components...")
+  (reagent/render [simple-component] (js/document.getElementById "app")))
+
+(init!)
