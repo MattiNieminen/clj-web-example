e1926cf613212a2f0b8ce6f6a6250e7f9e80df0e
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-05-30 14:35:44 +0300

    Bumped Clojure version.

--- app/project.clj
+++ app/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
                  [org.clojure/tools.logging "0.3.1"]
                  [ch.qos.logback/logback-classic "1.1.3"]
                  [com.stuartsierra/component "0.2.3"]
