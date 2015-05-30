f0a51b85eccad3977d07a675f4b46abd4fbf74b9
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-05-30 16:31:38 +0300

    Removed broken test created by Leiningen.

--- app/test/clj_web_example/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clj-web-example.core-test
-  (:require [clojure.test :refer :all]
-            [clj-web-example.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
