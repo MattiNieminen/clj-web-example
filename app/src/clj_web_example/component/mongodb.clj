fdd7fe34df7dbe5087a219055f2596903c27e619
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-05-31 00:33:40 +0300

    Implemented component for MongoDB.

---
+++ app/src/clj_web_example/component/mongodb.clj
@@ -0,0 +1,25 @@
+(ns clj-web-example.component.mongodb
+  (:require [com.stuartsierra.component :as component]
+            [clojure.tools.logging :as log]
+            [monger.core :as mg]))
+
+(defrecord MongoDb [config connection]
+  component/Lifecycle
+  
+  (start [this]
+    (let [mongodb-config (get-in config [:loaded :mongodb])
+          connection-config (:connection mongodb-config)
+          new-connection (mg/connect connection-config)]
+      (log/info "Connected to MongoDB @"
+                (str (:host connection-config) ":" (:port connection-config)))
+      (assoc this
+             :connection new-connection
+             :db (mg/get-db new-connection (:database mongodb-config)))))
+  
+  (stop [this]
+    (when connection (mg/disconnect connection))
+    (dissoc this :connection :db)))
+
+(defn new-mongodb
+  []
+  (map->MongoDb {}))
--- app/src/clj_web_example/system.clj
+++ app/src/clj_web_example/system.clj
@@ -1,5 +1,6 @@
 (ns clj-web-example.system
   (:require [clj-web-example.component.config :as config]
+            [clj-web-example.component.mongodb :as mongodb]
             [clj-web-example.component.httpkit :as httpkit]
             [com.stuartsierra.component :as component]))
 
@@ -7,4 +8,5 @@
   [profile routes]
   (component/system-map
     :config (config/new-config profile)
+    :mongodb (component/using (mongodb/new-mongodb) [:config])
     :httpkit (component/using (httpkit/new-httpkit routes) [:config])))
