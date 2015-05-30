c0c3e6f963f2a590579c7370c5eeb5c59cf20a5f
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-05-30 18:06:15 +0300

    Implemented a very simple navigation component.

--- app/src/clj_web_example/ui/main.cljs
+++ app/src/clj_web_example/ui/main.cljs
@@ -1,14 +1,17 @@
 (ns ^:figwheel-always clj-web-example.ui.main
   (:require [clj-web-example.localization :refer [tr]]
-            [reagent.core :as reagent]))
+            [reagent.core :as reagent]
+            [clj-web-example.ui.view.navigation :as navigation]))
 
-(defn simple-component []
+(defn main-view
+  []
   [:div
-   [:h1 (tr :hello)]])
+   [:h1 (tr :title)]
+   [navigation/navigation]])
 
 (defn init!
   []
   (.log js/console "Here we go! Rendering the components...")
-  (reagent/render [simple-component] (js/document.getElementById "app")))
+  (reagent/render [main-view] (js/document.getElementById "app")))
 
 (init!)
---
+++ app/src/clj_web_example/ui/view/navigation.cljs
@@ -0,0 +1,13 @@
+(ns clj-web-example.ui.view.navigation
+  (:require [clj-web-example.localization :refer [tr]]))
+
+(defn navigation-link
+  [content href]
+  [:a {:class "navigation-link"
+       :href href} content])
+
+(defn navigation
+  []
+  [:div#navigation
+   [navigation-link (tr :messages) "#messages"]
+   [navigation-link (tr :new-message) "#messages/new"]])
