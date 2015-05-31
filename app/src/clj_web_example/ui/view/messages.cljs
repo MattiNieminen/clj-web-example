95f6b217d490dc5729eeec90c993a6f49bc20d38
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-05-31 16:37:18 +0300

    Implemented view for showing messages.

--- app/src/clj_web_example/ui/view/messages.cljs
+++ app/src/clj_web_example/ui/view/messages.cljs
@@ -1,6 +1,31 @@
 (ns clj-web-example.ui.view.messages
-  (:require [clj-web-example.ui.view.view :as view]))
+  (:require [reagent.core :as reagent]
+            [clj-web-example.ui.utils :as utils]
+            [clj-web-example.ui.route :as route]
+            [clj-web-example.ui.view.view :as view]))
+
+(defonce messages-state (reagent/atom nil))
+
+(defn get-messages
+  []
+  (utils/http-get "/api/messages" #(reset! messages-state (:body %))))
+
+(defn message
+  [msg]
+  [:div
+   [:h3 (:sender msg)]
+   [:p (:body msg)]])
+
+(defn messages
+  []
+  [:div#content
+   [:h2 "Messages"]
+   [:div
+    (for [msg @messages-state]
+      ^{:key (:_id msg)} [message msg])]])
+
+(def messages-with-callback
+  (with-meta messages {:component-did-mount get-messages}))
 
 (defmethod view/render-view "messages" []
-  [:div#content
-   [:h2 "Here will be messages!"]])
+  [messages-with-callback])
